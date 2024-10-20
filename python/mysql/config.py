a78b71bc48398e6502d1bb5972a4bf1a0962f774
goustg62@gmail.com <goustg62@gmail.com>
2024-10-20 19:02:39 +0300

    commit

--- python/mysql/config.py
+++
@@ -1,4 +0,0 @@
-host = "localhost"         # host of db / now it's local host
-user = "root"              # user of db
-password = "2010Navolokov" # password of db
-db_name = "HackerStudio"   # db name
